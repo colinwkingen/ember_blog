52f236c73af9d9023fa094851d4d3b6d8e38f560
cory olson and Colin W Kingen <cory@>
2016-08-17 16:20:22 -0700

    add edits and new post

--- app/routes/admin/edit.js
+++ app/routes/admin/edit.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('article', params.article_id);
+  },
+  actions: {
+    editPost(article) {
+      article.save()
+    }
   }
 });
--- app/routes/admin/new.js
+++ app/routes/admin/new.js
@@ -1,5 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    
+  model() {
+    return this.store.findAll('article');
+  },
+  actions: {
+    newPost(myTitle, myAuthor, myCategory, myContent, myImage, myTimestamp) {
+      var inputs = {
+        title: myTitle,
+        author: myAuthor,
+        category: myCategory,
+        content: myContent,
+        image: myImage ? myImage : "https://placeholdit.imgix.net/~text?txtsize=33&txt=373%C3%97280&w=684&h=513",
+        timestamp: myTimestamp
+      };
+      console.log(inputs);
+      var newArticle = this.store.createRecord('article', inputs);
+      newArticle.save();
+      // this.sendAction('realNewPost', inputs);
+    },
+    // realNewPost(params) {
+    //   var newArticle = this.store.createRecord('article', inputs);
+    //   newArticle.save();
+    // }
+  }
 });
